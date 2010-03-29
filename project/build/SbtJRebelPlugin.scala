9f80d0b1e97bd0c4a905ad95347be017e39da963
Joonas Javanainen <joonas.javanainen@gmail.com>
2010-03-29 14:43:09 +0300

    New: Added publish configuration

--- project/build/SbtJRebelPlugin.scala
+++ project/build/SbtJRebelPlugin.scala
@@ -1,4 +1,13 @@
 import sbt._
 
 class SbtJRebelPlugin(info: ProjectInfo) extends PluginProject(info) {
+
+  override def managedStyle = ManagedStyle.Maven
+
+  val publishTo = if (version.toString.endsWith("-SNAPSHOT"))
+    ("Nexus" at "https://www.jawsy.fi/nexus/content/repositories/snapshots")
+    else ("Nexus" at "https://www.jawsy.fi/nexus/content/repositories/releases")
+
+  Credentials(Path.userHome / ".ivy2" / "jawsy.credentials", log)
+
 }

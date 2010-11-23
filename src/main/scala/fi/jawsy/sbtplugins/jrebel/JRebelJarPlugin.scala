5ce0de070875e18646c71391778c25df61a441ca
Joonas Javanainen <joonas.javanainen@gmail.com>
2010-11-23 21:59:15 +0200

    Fix: In JRebelJarPlugin, override also compileAction
    + Fixes automatic rebel.xml generation in JAR projects that have no resource files
      in src/main/resources

--- src/main/scala/fi/jawsy/sbtplugins/jrebel/JRebelJarPlugin.scala
+++ src/main/scala/fi/jawsy/sbtplugins/jrebel/JRebelJarPlugin.scala
@@ -5,6 +5,7 @@ import sbt._
 trait JRebelJarPlugin extends DefaultProject with JRebelPlugin {
 
   override def copyResourcesAction = super.copyResourcesAction dependsOn(generateRebelXml)
+  override def compileAction = super.compileAction dependsOn(generateRebelXml)
 
   override def packagePaths = if (packageRebelXml) super.packagePaths +++ ((rebelOutputPath ##) ***) else super.packagePaths
 

179e0aff7b783bac42fc4123fe1ebf8466941add
Joonas Javanainen <joonas.javanainen@gmail.com>
2011-06-09 15:08:34 +0100

    New: More SBT 0.9/0.10 work

--- src/main/scala/JRebelPlugin.scala
+++ src/main/scala/JRebelPlugin.scala
@@ -2,14 +2,24 @@ package fi.jawsy.sbtplugins.jrebel
 
 import sbt._
 import sbt.CommandSupport.logger
+import sbt.Keys._
 import scala.xml._
 
 object JRebelPlugin extends Plugin {
 
-  override lazy val settings =
-    (Seq(Keys.commands += generateRebelXml))
+  lazy val jrebelDirectory = SettingKey[File]("jrebel-directory", "Output directory for rebel.xml")
+  lazy val jrebelXml = SettingKey[File]("jrebel-xml", "Path for generated rebel.xml")
+  lazy val jrebelClasspath = SettingKey[Seq[File]]("jrebel-classpath", "Reloadable JRebel classpath")
 
-  lazy val jrebelInUse = List("jrebel.lic", "javarebel.lic").exists(this.getClass.getClassLoader.getResource(_) != null)
+  lazy val jrebelConfig: Seq[Setting[_]] = Seq(
+    jrebelDirectory <<= resourceManaged / "jrebel",
+    jrebelXml <<= jrebelDirectory / "rebel.xml",
+    jrebelClasspath <<= Seq(classDirectory in Compile, classDirectory in Test).join
+  )
+
+  override lazy val settings =
+    (Seq(commands += generateRebelXml)) ++ jrebelConfig
+
   lazy val generateRebelXml =
     Command.command("generate-rebel-xml") { state =>
       val extracted = Project.extract(state)
@@ -18,22 +28,16 @@ object JRebelPlugin extends Plugin {
       def setting[A](key: SettingKey[A], configuration: Configuration = Compile): Option[A] = key in (currentRef, configuration) get
         structure.data
 
-      def xml(dir: File) = if (dir.exists) <dir name={ dir.absolutePath } /> else NodeSeq.Empty
+      def dirXml(dir: File) = <dir name={ dir.absolutePath } />
 
-      val compileClasses = setting(Keys.classDirectory).map(xml)
-      val testClasses = setting(Keys.classDirectory, Test).map(xml)
-
-      val outputDirectory = setting(Keys.generatedResourceDirectory)
-
-      (for (outputDirectory <- setting(Keys.generatedResourceDirectory)) yield {
+      (for (rebelXml <- setting(jrebelXml);
+            jrebelClasspath <- setting(jrebelClasspath)) yield {
         val xml =
           <application xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.zeroturnaround.com" xsi:schemaLocation="http://www.zeroturnaround.com/alderaan/rebel-2_0.xsd">
             <classpath>
-             { compileClasses.toList ::: testClasses.toList }
+             { jrebelClasspath.map(dirXml).toList }
             </classpath>
           </application>
-
-        val rebelXml = outputDirectory / "rebel.xml"
 
         IO.touch(rebelXml)
         XML.save(rebelXml.absolutePath, xml, "UTF-8", true)

b678b698c19bd9e624c2cf3155f9891413a4cf02
Joonas Javanainen <joonas.javanainen@gmail.com>
2011-10-01 08:03:17 +0300

    New: Refactorings
    + Plugin key namespacing
    + JRebel 4.5 -compatible jrebel detection (this approach is much better
    	than the previous one)

--- src/main/scala/JRebelPlugin.scala
+++ src/main/scala/JRebelPlugin.scala
@@ -7,52 +7,54 @@ import sbt.Scope.GlobalScope
 import scala.xml._
 
 object JRebelPlugin extends Plugin {
-  val jrebelEnabled = SettingKey[Boolean]("jrebel-enabled")
-  val jrebelClasspath = SettingKey[Seq[File]]("jrebel-classpath")
-  val jrebelGenerate = TaskKey[Seq[File]]("jrebel-generate")
-  val jrebelXml = SettingKey[File]("jrebel-xml")
-  val jrebelWebLinks = SettingKey[Seq[File]]("jrebel-web-links")
+  object jrebel {
+    val classpath = SettingKey[Seq[File]]("jrebel-classpath")
+    val enabled = SettingKey[Boolean]("jrebel-enabled")
+    val rebelXml = SettingKey[File]("jrebel-rebel-xml")
+    val webLinks = SettingKey[Seq[File]]("jrebel-web-links")
+  }
 
-  lazy val jrebelSettings: Seq[Project.Setting[_]] = Seq[Setting[_]](
-    jrebelEnabled := List("jrebel.lic", "javarebel.lic").exists(this.getClass.getClassLoader.getResource(_) != null),
-    jrebelClasspath <<= Seq(Keys.classDirectory in Compile, Keys.classDirectory in Test).join,
-    jrebelWebLinks := Seq(),
-    jrebelXml <<= (resourceManaged in Compile) { _ / "rebel.xml" },
-    jrebelGenerate <<= jrebelXmlTask,
+  val jrebelGenerate = TaskKey[Seq[File]]("jrebel-generate")
+
+  val jrebelSettings: Seq[Project.Setting[_]] = Seq[Setting[_]](
+    jrebel.classpath <<= Seq(Keys.classDirectory in Compile, Keys.classDirectory in Test).join,
+    jrebel.enabled := (java.lang.Package.getPackage("com.zeroturnaround.javarebel") != null),
+    jrebel.rebelXml <<= (resourceManaged in Compile) { _ / "rebel.xml" },
+    jrebel.webLinks := Seq(),
+    jrebelGenerate <<= rebelXmlTask,
     resourceGenerators in Compile <+= jrebelGenerate.identity
   )
 
-  private def toXml(dir: File) = <dir name={ dir.absolutePath } />
+  private def dirXml(dir: File) = <dir name={ dir.absolutePath } />
 
   private def webLinkXml(link: File) =
     <web>
       <link>
-      { toXml(link) }
+      { dirXml(link) }
       </link>
     </web>
 
-  private def jrebelXmlTask: Project.Initialize[Task[Seq[File]]] =
-    (jrebelEnabled, jrebelClasspath, jrebelXml, jrebelWebLinks, state) map {
-      (jrebelEnabled, jrebelClasspath, jrebelXml, jrebelWebLinks, state) =>
-        if (!jrebelEnabled) Nil
+  private def rebelXmlTask: Project.Initialize[Task[Seq[File]]] =
+    (jrebel.enabled, jrebel.classpath, jrebel.rebelXml, jrebel.webLinks, state) map {
+      (enabled, classpath, rebelXml, webLinks, state) =>
+        if (!enabled) Nil
         else {
-          println(jrebelWebLinks)
           val xml =
             <application xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.zeroturnaround.com" xsi:schemaLocation="http://www.zeroturnaround.com/alderaan/rebel-2_0.xsd">
               <classpath>
-               { jrebelClasspath.map(toXml) }
+               { classpath.map(dirXml) }
               </classpath>
               {
-                jrebelWebLinks.map(webLinkXml)
+                webLinks.map(webLinkXml)
               }
             </application>
 
-          IO.touch(jrebelXml)
-          XML.save(jrebelXml.absolutePath, xml, "UTF-8", true)
+          IO.touch(rebelXml)
+          XML.save(rebelXml.absolutePath, xml, "UTF-8", true)
 
-          logger(state).info("Wrote rebel.xml to %s".format(jrebelXml.absolutePath))
+          logger(state).info("Wrote rebel.xml to %s".format(rebelXml.absolutePath))
 
-          jrebelXml :: Nil
+          rebelXml :: Nil
         }
     }
 }
